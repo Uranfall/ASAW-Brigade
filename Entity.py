3dc94cc67fe9eefc1f3577cd5c340ff8844a0784
Bokmok <ifalkon7@gmail.com>
2026-03-19 17:56:46 +0200

    Update Entity.py

--- Entity.py
+++ Entity.py
@@ -1,10 +1,11 @@
 
 class Entity:
-    def __init__(self, position: tuple[int, int], rotation: float):
+    def __init__(self, position: tuple[int, int], rotation: float, seeThrough):
         self.position = position
         self.rotation = rotation
         self.collision_points = []
         self.drawing_objects = []
+        self.seeThrough = SeeThrough
 
 
 
