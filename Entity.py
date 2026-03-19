d125860c7faeb29704ffa03aba81939cff7a0e99
NickLempert <nick209s21@gmail.com>
2026-03-19 18:11:30 +0200

    some small changes

--- Entity.py
+++ Entity.py
@@ -2,12 +2,16 @@
 class Entity:
     #seeThrough - Boolean that checks if you can see through the entity or not, used for ray logic
     #collision - if a unit can go through the object or not
-    def __init__(self, position: tuple[int, int], rotation: float, seeThrough, collision):
+    def __init__(self,
+                 position: tuple[int, int],
+                 rotation: float,
+                 see_through=False,
+                 collision=True):
         self.position = position
         self.rotation = rotation
         self.collision_points = []
         self.drawing_objects = []
-        self.seeThrough = seeThrough
+        self.see_through = see_through
         self.collision = collision
 
 
