f4d9f3c365148917541c4cf223e9ca55735deff5
Bokmok <ifalkon7@gmail.com>
2026-03-30 23:18:57 +0300

    Update logic_utility.py

--- logic_utility.py
+++ logic_utility.py
@@ -1,7 +1,9 @@
 from Entity import Entity
-#first make a plan how you are gonna cehck eahc one thing
+#navigation: UNIT -> NavNode -> find a path between them -> stop at closet NavNode from the click that isnt abstracted -> walk towards coordinates
 
 
 def calculate_route_from(entity: Entity, target: Entity, game_state) -> list[tuple[int, int]]:
     ...
+class NavNode:
+    def __init__(self, position: tuple[int,int])
 
