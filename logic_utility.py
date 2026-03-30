c4902921d9c6b7eaf9b58941fef5c8085ab81d15
Bokmok <ifalkon7@gmail.com>
2026-03-30 20:51:46 +0300

    Update logic_utility.py

--- logic_utility.py
+++ logic_utility.py
@@ -1,4 +1,5 @@
 from Entity import Entity
+#first make a plan how you are gonna cehck eahc one thing
 
 
 def calculate_route_from(entity: Entity, target: Entity, game_state) -> list[tuple[int, int]]:
