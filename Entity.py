7458f88bdc33a753f1781ee2543b51474b783fb4
NickLempert <nick209s21@gmail.com>
2026-04-01 19:44:50 +0300

    Added optimization to graphics. We might still decide to use shapes instead of images because they are faster.

--- Entity.py
+++ Entity.py
@@ -7,8 +7,13 @@ from graphics_utility import Camera
 
 
 class Entity:
-    IMAGE: PathLike = "./Sprites/PlaceHolders/place_holder.png"
+    IMAGE: pygame.image = pygame.image.load("./Sprites/PlaceHolders/place_holder.png")
     IMAGE_SCALE = 0.5
+    COLOR = (100, 100, 70)
+    SIMPLIFY_AT = 40  # Draw a simple shape instead of the image if smaller than.
+    DRAW_SHAPE: pygame.draw = pygame.draw.circle
+    SHAPE_SIZE_ADJUST = 0.3
+    STATIC_ROTATION = True
 
     #seeThrough - Boolean that checks if you can see through the entity or not, used for ray logic
     #collision - if a unit can go through the object or not
@@ -23,6 +28,11 @@ class Entity:
         self.see_through = see_through
         self.collision = collision
 
+        # The following variables are only for graphics.
+        self.zoom_render: tuple[float, pygame.image] | None = None
+        self.rotated_picture = pygame.transform.rotate(self.__class__.IMAGE, self.rotation) \
+            if self.__class__.STATIC_ROTATION else None
+
     def is_colliding_with(self, entity: Entity) -> bool:
         ...
 
@@ -34,9 +44,27 @@ class Entity:
 
     def draw(self, screen: pygame.display, camera: Camera):
         scale = camera(self.__class__.IMAGE_SCALE)
-        img = pygame.image.load(self.__class__.IMAGE)
+        img = self.__class__.IMAGE
+
         dimensions = (img.get_width()*scale, img.get_height()*scale)
-        img = pygame.transform.rotate(img, self.rotation)
-        img = pygame.transform.scale(img, dimensions)
         new_pos = camera(self.position[0], self.position[1])
-        screen.blit(img, (new_pos[0] - dimensions[0]/2, new_pos[1] - dimensions[1]/2))
+        if sum(dimensions) < self.__class__.SIMPLIFY_AT:
+            self.DRAW_SHAPE(screen, self.__class__.COLOR, new_pos, max(dimensions)*self.__class__.SHAPE_SIZE_ADJUST)
+            return
+
+        corner = (new_pos[0] - dimensions[0]/2, new_pos[1] - dimensions[1]/2)
+        if not (-dimensions[0] < corner[0] < screen.get_width()+dimensions[0] and
+                -dimensions[1] < corner[1] < screen.get_height()+dimensions[1]):
+            return
+
+        if self.rotated_picture is None:
+            img = pygame.transform.rotate(img, self.rotation)
+        else:
+            img = self.rotated_picture
+        if self.zoom_render is not None\
+                and camera.zoom == self.zoom_render[0]:
+            img = self.zoom_render[1]
+        else:
+            img = pygame.transform.scale(img, dimensions)
+            self.zoom_render = (camera.zoom, img)
+        screen.blit(img, corner)
