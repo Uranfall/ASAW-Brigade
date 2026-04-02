4fcd99118c976a96c1d5269e208cf10f1ec4a7d8
NickLempert <nick209s21@gmail.com>
2026-04-02 18:23:47 +0300

    Added some utilities and debug entities.

--- shared_utility.py
+++ shared_utility.py
@@ -1,5 +1,14 @@
+import math
 
 
 def sign(num: float):
-    return -1 if num < 1 else (1 if num > 0 else 0)
+    return -1 if num < 1 else (1 if num > 0 else 0
+
+
+def angle_to_vector(angle: float, distance=1.0):
+    return math.sin(math.radians(angle))*distance, math.cos(math.radians(angle))*distance
+
+
+def vector_to_angle(vector: list[float, float]):
+    return math.degrees(math.atan2(*vector[::-1]))
 
