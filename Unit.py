a58f2f7a63c283c9a9a7cd673a77bff9af876053
Bokmok <ifalkon7@gmail.com>
2026-04-02 14:41:31 +0300

    Update Unit.py

--- Unit.py
+++ Unit.py
@@ -3,8 +3,13 @@ from Entity import Entity
 
 class Unit(Entity):
     STATIC_ROTATION = False
-
-    def __init__(self, position: tuple[int, int], target_pos: tuple[int,int], rotation: float, speed: int):
+    
+    def __init__(self, 
+                 position: tuple[int, int], 
+                 target_pos: tuple[int,int], 
+                 rotation: float, 
+                 speed: int, 
+                 action_queue: list[] ):
         super().__init__(position, rotation)
     
 
