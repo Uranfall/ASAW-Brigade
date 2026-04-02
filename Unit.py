11a64b4b750ae5f552814a25898d10235f43542e
Bokmok <ifalkon7@gmail.com>
2026-04-02 16:15:13 +0300

    Update Unit.py

--- Unit.py
+++ Unit.py
@@ -8,10 +8,22 @@ class Unit(Entity):
     def __init__(self, 
                  position: tuple[int, int], 
                  target_pos: tuple[int,int], 
-                 rotation: float, 
+                 rotation: float,
+                 target_rotation: float,
                  speed: int, 
                  action_queue: list[] ):
         super().__init__(position, rotation)
+
+    def move():
+    ...
+    def fire():
+    ...
+    def interact():
+    ...
+    def idle():
+    ...
+    
+    
 
     
     
