213ccc2b573c0f0572a23ef874e47f9f3de8718b
NickLempert <nick209s21@gmail.com>
2026-05-06 20:55:52 +0300

    Unit creation buttons

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import pygame
 
 import DebugGlobal
 from Entities.Units import Mouse
+from GlobalVariables import TEXT_RED_CURVE
 from Unit_AI import a_star
+from graphics.UI_Entities import Button, Text
 from shared_utility import get_closest_node
 from VFX import Explosion
 from graphics.Debug_Entities import DebugRay, DebugLine
@@ -20,7 +22,32 @@ from GameData import GameDataLocal
 def main(screen=pygame.display.set_mode((500, 500), pygame.RESIZABLE)):
     #path = a_star(start, goal, grid) for when you finish playing deadlock: this is for creating the path
 
+    def quit_main():
+        nonlocal run
+        run = False
+
+    def create_mouse():
+        pass
+
+    def create_soldier():
+        pass
+
+    def create_uuuhh_idk____oh_wait_tank__right():
+        pass
+
     ui_data = UIData(screen)
+    exit_button = Button((0, 220), (100, 40), Text((0, 0), 0, 'Quit', TEXT_RED_CURVE), action=quit_main)
+    exit_button.creation_time = 0
+    ui_data.add_on_screen_entity(exit_button)
+    c_b1 = Button((-130, -220), (100, 40), Text((0, 0), 0, 'Mouse', TEXT_RED_CURVE, size=30), action=create_mouse)
+    c_b2 = Button((0, -220), (140, 40), Text((0, 0), 0, 'Soldier', TEXT_RED_CURVE, size=30), action=create_soldier)
+    c_b3 = Button((130, -220), (100, 40), Text((0, 0), 0, 'Tank', TEXT_RED_CURVE, size=30),
+                  action=create_uuuhh_idk____oh_wait_tank__right)
+    ui_data.add_on_screen_entity(c_b1)
+    ui_data.add_on_screen_entity(c_b2)
+    ui_data.add_on_screen_entity(c_b3)
+    ui_data.add_on_screen_entity(Text((0, -180), 0, 'Creation Menu:', TEXT_RED_CURVE))
+
     logic_data = LOGIC_DATA()
     DebugGlobal.ui_data = ui_data
     player_team = 0
