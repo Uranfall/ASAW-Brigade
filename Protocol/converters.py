c9162eee1c7157298af3c406c40694924e15999a
NickLempert <nick209s21@gmail.com>
2026-05-10 22:59:25 +0300

    fixed from string

--- Protocol/converters.py
+++ Protocol/converters.py
@@ -2,9 +2,10 @@ from Entity import Entity
 
 
 def string_to_entity(string: str) -> Entity:
-    list_of_stats = string[1:-1].split(';')
+    print(string)
+    list_of_stats = string.split(';')
     stats = dict()
     for key, value in map(lambda stat: (stat[:3], stat[3:]), list_of_stats):
         stats.update({key: value})
-    entity = eval(stats['cls']+f'({stats["pos"]}, {stats["rot"]}, unique_id={stats["uid"]})')
+    entity = eval(stats['cls']+f'(({stats["pos"]}), {stats["rot"]}, unique_id={stats["uid"]})')
     return entity
